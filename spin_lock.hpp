1239f72420f45346084c4815f3bc19be2aee13aa
Ian Forbes <ian@forbes.io>
2018-03-08 22:33:21 -0500

    Added spinlock with exponential backoff.

--- spin_lock.hpp
+++ spin_lock.hpp
@@ -7,21 +7,38 @@ class spin_lock {
     std::atomic_flag _flag = ATOMIC_FLAG_INIT;
 public:
     void lock() {
-        while (_flag.test_and_set(std::memory_order_acquire));
-    }
-
-    template <typename F, typename... Args>
-    void lock(F func, Args... args) {
-        while (_flag.test_and_set(std::memory_order_acquire)) {
-            F(std::forward<Args...>(args...));
-        }
+        while (_flag.test_and_set(std::memory_order_acq_rel))
+            asm volatile("pause");
     }
 
     bool try_lock() {
-        return !_flag.test_and_set(std::memory_order_acquire);
+        return !_flag.test_and_set(std::memory_order_acq_rel);
     }
 
     void unlock() {
         _flag.clear(std::memory_order_release);
     }
 };
+
+class spin_lock_backoff {
+    std::atomic_flag _flag = ATOMIC_FLAG_INIT;
+public:
+    void lock() {
+        unsigned backoff = 0;
+        while (_flag.test_and_set(std::memory_order_acq_rel)) {
+            for(unsigned i = 0; i < (1u << backoff); i++) {
+                asm volatile("pause");
+            }
+            backoff++;
+        }
+    }
+
+    bool try_lock() {
+        return !_flag.test_and_set(std::memory_order_acq_rel);
+    }
+
+    void unlock() {
+        _flag.clear(std::memory_order_release);
+    }
+};
+
